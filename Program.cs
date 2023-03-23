6379f03f04f6fd785ae19d338fd3cc6cb04b94d8
Andrei Borodin <andrei.borod1n@mail.ru>
2023-03-23 21:33:08 +0500

    Edit program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
-﻿string[] array = {"hello", "privet", "231", "pr", "hey"};
+﻿Console.Write("Первоначальный массив: ");
+string[] array = {"hel", "privet", "2312", "pr", "hey"};
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine(" ");
 void ArrayThreeNum(string[] matr)
 {
+string[] newArray = new string[3];
     for (int i = 0; i < matr.Length; i++)
     {
+        int l = 0;
         if(matr[i].Length <= 3)
         {
-            Console.Write($"{matr[i]} ");
+            newArray[l] = matr[i];
+            Console.Write($"{newArray[l]} ");
+            l++;
         }
     }
 }
-Console.WriteLine("Строки, число символов в которых равно или менее трёх: ");
+Console.WriteLine("Новый массив из строк старого массива, в которых число символов не превышает 3: ");
 ArrayThreeNum(array);
