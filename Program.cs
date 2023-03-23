34190fd83f692b32dae1d3a0176646609b1bb004
Andrei <andrei.borod1n@mail.ru>
2023-03-23 20:40:40 +0500

    Add method ArrayThreeNum

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+void FillArray(string[] matr)
+{
+    for (int i = 0; i < matr.Length; i++)
+    {
+        matr[i] = Console.ReadLine();
+    }
+}
+FillArray(array);
+void ArrayThreeNum(string[] matr)
+{
+    for (int i = 0; i < matr.Length; i++)
+    {
+        if(matr[i].Length <= 3)
+        {
+            Console.WriteLine(matr[i]);
+        }
+    }
+}
+ArrayThreeNum(array);
